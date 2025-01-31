cd91de825906b8c22453f3922e693888ee34892d
alizadamurad <elizademurad03@gmail.com>
2025-02-01 03:03:51 +0400

    export update

--- connectMongo.js
+++ connectMongo.js
@@ -4,11 +4,16 @@ const connectDB = async () => {
     try {
         await mongoose.connect(process.env.MONGO_DB_URL)
 
-        const cryptoConnection = await mongoose.createConnection(process.env.MONGO_CRYPTO_URL)
+        const cryptoConnection = mongoose.createConnection(process.env.MONGO_CRYPTO_URL)
         console.log('MongoDB Connected');
+
+        module.exports = {
+            userDB: mongoose.connection,
+            cryptoDB: cryptoConnection,
+        };
     } catch (error) {
         console.error('MongoDB Connection Error:', error)
     }
 
 }
-module.exports = { userDB: mongoose.connection, cryptoDB: cryptoConnection }
+// module.exports = { userDB: mongoose.connection, cryptoDB: cryptoConnection }
